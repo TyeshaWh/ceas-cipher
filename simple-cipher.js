d8eabbe2770c04b79bf89d803674fdd1ace15b7a
User <user@hackerrank.workspace>
2023-06-14 15:14:06 +0000

    Add initial repository
    Commit by HackerRank

---
+++ simple-cipher.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Simple Cipher' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Cipher {
+  constructor() {
+    // todo: implement
+  }
+
+  encode() {
+    // todo: implement
+  }
+
+  decode() {
+    // todo: implement
+  }
+
+  get key() {
+    // todo: implement
+  }
+}

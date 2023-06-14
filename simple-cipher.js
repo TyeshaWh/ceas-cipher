2215f186379ad33331fe02b7c9c6782847252ed6
Admin <admin.push@hackerrank.workspace>
2023-06-14 15:59:40 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- simple-cipher.js
+++ simple-cipher.js
@@ -3,16 +3,30 @@
 // convenience to get you started writing code faster.
 //
 
+import { stringify } from "querystring"
+
 export class Cipher {
   constructor() {
     // todo: implement
   }
+  /**
+   * step 1 take in string and move every letter 3 letters to the left.
+   */
 
   encode() {
-    // todo: implement
+    /** todo: take in input from user
+        todo: loop every character in provided string
+  for loop 
+        todo: get letter from stringify
+        todo: convert to number
+        todo: do subtraction
+        todo: print letter
+  return
   }
 
   decode() {
+      /** step 1- take in input from user
+       *  step 2- loop every character in string
     // todo: implement
   }
 

fb83787e9300cfbedb187ea9249064ca289de5dd
Admin <admin.push@hackerrank.workspace>
2023-06-15 21:04:58 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- simple-cipher.js
+++ simple-cipher.js
@@ -22,13 +22,16 @@ export class Cipher {
         todo: do subtraction
         todo: print letter
   return output */
-  let alphaBet  = {
-      ('a':'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+  const alphaBet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+  SVGComponentTransferFunctionElement.log(alphaBet)
+
+  {
+     
 };
       
 
-  let forward = alphaBet + 4
-  
+
 
   }
 

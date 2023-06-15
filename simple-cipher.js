67675a95c2d92d27f5f7410b2897b97d0167f649
Admin <admin.push@hackerrank.workspace>
2023-06-15 20:47:56 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- simple-cipher.js
+++ simple-cipher.js
@@ -21,16 +21,32 @@ export class Cipher {
         todo: convert to number
         todo: do subtraction
         todo: print letter
-  return
+  return output */
+  let alphaBet = = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+      
+
+  let forward = alphaBet + 4
+  
+
   }
 
   decode() {
-      /** step 1- take in input from user
-       *  step 2- loop every character in string
-    // todo: implement
+        /** todo: take in input from user
+        todo: loop every character in provided string
+  for loop 
+        todo: get letter from stringify
+        todo: convert to number
+        todo: do subtraction
+        todo: print letter
+  return output */
   }
 
   get key() {
     // todo: implement
+    /**
+     * 
+     * 
+     * 
+     */
   }
 }
